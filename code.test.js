50733ef2595794afac9f77b320f3a1e49494748f
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-05-10 22:46:50 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -4,5 +4,7 @@ eval(fs.readFileSync('code.js')+'');
 var TestArr=[4,2,2,2,3,3,1,1,1]
 
 console.assert(KeyCountAsync(TestArr,4).then((outcome)=>{ console.log(outcome==1)})==true)
-
-
+console.assert(KeyCountAsync(TestArr,3).then((outcome)=>{ console.log(outcome==2)})==true)
+console.assert(KeyCountAsync(TestArr,2).then((outcome)=>{ console.log(outcome==3)})==true)
+console.assert(KeyCountAsync(TestArr,1).then((outcome)=>{ console.log(outcome==3)})==true)
+console.assert(KeyCountAsync(TestArr,5).then((outcome)=>{ console.log(outcome==0)})==true)

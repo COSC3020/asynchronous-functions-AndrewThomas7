32543d200cbd2240285f0f67cab06962b815d2b5
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-05-10 23:23:44 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -8,3 +8,11 @@ console.assert(KeyCountAsync(TestArr,3).then((outcome)=>{ console.log(outcome==2
 console.assert(KeyCountAsync(TestArr,2).then((outcome)=>{ console.log(outcome==3)})==true)
 console.assert(KeyCountAsync(TestArr,1).then((outcome)=>{ console.log(outcome==3)})==true)
 console.assert(KeyCountAsync(TestArr,5).then((outcome)=>{ console.log(outcome==0)})==true)
+
+var TestArr2=['a','a','c','a','a','f','c','f','e']
+
+console.assert(KeyCountAsync(TestArr2,'a').then((outcome)=>{console.log(outcome==4)})==true)
+console.assert(KeyCountAsync(TestArr2,'c').then((outcome)=>{console.log(outcome==2)})==true)
+console.assert(KeyCountAsync(TestArr2,'f').then((outcome)=>{console.log(outcome==2)})==true)
+console.assert(KeyCountAsync(TestArr2,'e').then((outcome)=>{console.log(outcome==1)})==true)
+console.assert(KeyCountAsync(TestArr2,1).then((outcome)=>{console.log(outcome==0)})==true)

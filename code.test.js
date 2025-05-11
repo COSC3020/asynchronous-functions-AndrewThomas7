3891aa59df5bf7fe3ab23b6aa47b94b2e931f299
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-05-10 22:44:45 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,1 +1,8 @@
+const fs = require('fs');
+const assert = require('assert');
+eval(fs.readFileSync('code.js')+'');
+var TestArr=[4,2,2,2,3,3,1,1,1]
+
+console.assert(KeyCountAsync(TestArr,4).then((outcome)=>{ console.log(outcome==1)})==true)
+
 

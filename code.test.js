a6bd65acd952ebd796d78bbb76b3e0b88350117e
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-05-11 00:29:55 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -16,3 +16,14 @@ console.assert(KeyCountAsync(TestArr2,'c').then((outcome)=>{console.log(outcome=
 console.assert(KeyCountAsync(TestArr2,'f').then((outcome)=>{console.log(outcome==2)})==true)
 console.assert(KeyCountAsync(TestArr2,'e').then((outcome)=>{console.log(outcome==1)})==true)
 console.assert(KeyCountAsync(TestArr2,1).then((outcome)=>{console.log(outcome==0)})==true)
+
+
+var TestArr3=[1,1,1,1,1,1,1,1,]
+console.assert(KeyCountAsync(TestArr3,1).then((outcome)=>{console.log(outcome==8)})==true)
+
+var TestArr4=[19,4,2,15,6,7,'a','a','b',19,15,7,7,7,6,2,2,4,4,4]
+console.assert(KeyCountAsync(TestArr4,19).then((outcome)=>{console.log(outcome==2)})==true)
+console.assert(KeyCountAsync(TestArr4,4).then((outcome)=>{console.log(outcome==4)})==true)
+console.assert(KeyCountAsync(TestArr4,19).then((outcome)=>{console.log(outcome==2)})==true)
+console.assert(KeyCountAsync(TestArr4,7).then((outcome)=>{console.log(outcome==4)})==true)
+console.assert(KeyCountAsync(TestArr4,'a').then((outcome)=>{console.log(outcome==2)})==true)
